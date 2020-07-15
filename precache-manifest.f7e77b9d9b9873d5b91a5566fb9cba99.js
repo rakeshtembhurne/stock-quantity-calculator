d2ca729cd2b9f6d8366ac4e34c293734934de1a1
Rakesh Tembhurne <tj2point0@gmail.com>
2020-07-15 17:42:26 +0530

    Updates

---
+++ precache-manifest.f7e77b9d9b9873d5b91a5566fb9cba99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6696a30a45b86524f3a7cbd078830fc",
+    "url": "/stock-quantity-calculator/index.html"
+  },
+  {
+    "revision": "07cfefa9827c908cea15",
+    "url": "/stock-quantity-calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a05e41e82e70c1f3c281",
+    "url": "/stock-quantity-calculator/static/css/main.7a82dccc.chunk.css"
+  },
+  {
+    "revision": "07cfefa9827c908cea15",
+    "url": "/stock-quantity-calculator/static/js/2.b07dfd24.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/stock-quantity-calculator/static/js/2.b07dfd24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05e41e82e70c1f3c281",
+    "url": "/stock-quantity-calculator/static/js/main.bcb04346.chunk.js"
+  },
+  {
+    "revision": "f32061aaff5bde8a3fdc",
+    "url": "/stock-quantity-calculator/static/js/runtime-main.5a53796b.js"
+  }
+]);
