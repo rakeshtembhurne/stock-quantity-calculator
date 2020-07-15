06bf82d58ef02d05052af387d5e94325624bed08
Rakesh Tembhurne <tj2point0@gmail.com>
2020-07-15 17:33:36 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Row, Col, Button, Container } from 'react-bootstrap';
+
+export function useFormFields(initialState) {
+  const [fields, setValues] = React.useState(initialState);
+
+  function setFormField(event) {
+    const idOrName =
+      event.target.type === "radio" ? event.target.name : event.target.id;
+
+    setValues({
+      ...fields,
+      [idOrName]: event.target.value,
+    });
+  }
+
+  return [fields, setFormField];
+}
 
 function App() {
+
+  const [quantity, setQuantity] = React.useState(0);
+
+  const [fields, handleFieldChange] = useFormFields({
+    capital: 200000,
+    entryPrice: 0,
+    slPerTrade: 2,
+    stopLoss: 0,
+  });
+
+  function calculateQuantity(e) {
+    console.log("calculating");
+    e.preventDefault();
+
+    if (fields.capital && fields.entryPrice && fields.slPerTrade && fields.stopLoss) {
+      const maxLoss = ((fields.capital * fields.slPerTrade) / 100);
+      const stopLoss = Math.abs((fields.entryPrice - fields.stopLoss));
+      const quantity =  Math.round(maxLoss / stopLoss);
+      console.log({maxLoss, stopLoss, quantity});
+      setQuantity(quantity);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Row className="quantity-container">
+        <Col>{quantity}</Col>
+      </Row>
+      <Form onSubmit={calculateQuantity}>
+        <Form.Row>
+          <Form.Group as={Col}>
+            <Form.Label>Capital</Form.Label>
+            <Form.Control
+              placeholder="Capital"
+              size="lg"
+              autoFocus
+              autoComplete="off"
+              type="number"
+              id="capital"
+              value={fields.capital}
+              onChange={handleFieldChange}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group as={Col}>
+            <Form.Label>Stop Loss % per Trade</Form.Label>
+            <Form.Control
+              placeholder="SL % per trade"
+              size="lg"
+              autoFocus
+              autoComplete="off"
+              type="number"
+              id="slPerTrade"
+              value={fields.slPerTrade}
+              onChange={handleFieldChange}
+              required
+            />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+          <Form.Group as={Col}>
+            <Form.Label>Entry Price</Form.Label>
+            <Form.Control
+              placeholder="Entry Price"
+              size="lg"
+              autoFocus
+              autoComplete="off"
+              type="number"
+              id="entryPrice"
+              value={fields.entryPrice}
+              onChange={handleFieldChange}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group as={Col}>
+            <Form.Label>Stop Loss</Form.Label>
+            <Form.Control
+              placeholder="Capital"
+              size="lg"
+              autoFocus
+              autoComplete="off"
+              type="number"
+              id="stopLoss"
+              value={fields.stopLoss}
+              onChange={handleFieldChange}
+              required
+            />
+          </Form.Group>
+        </Form.Row>
+
+        <Button size="lg" variant="primary" type="submit">
+          Calculate Quantity
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
