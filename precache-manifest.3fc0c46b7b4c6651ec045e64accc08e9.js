984e21a350af78f6bb70bb9ef9d8df88e2e4bfd7
Rakesh Tembhurne <tj2point0@gmail.com>
2020-07-29 22:54:52 +0530

    Updates

---
+++ precache-manifest.3fc0c46b7b4c6651ec045e64accc08e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0b009c6f9a8cc9c1067955c5121243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c40cd15731139f742a4e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8e90b64afabfa919a34a",
+    "url": "/static/css/main.7a82dccc.chunk.css"
+  },
+  {
+    "revision": "c40cd15731139f742a4e",
+    "url": "/static/js/2.e74ccd68.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e74ccd68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e90b64afabfa919a34a",
+    "url": "/static/js/main.b4ef6289.chunk.js"
+  },
+  {
+    "revision": "06969524a64ff1bbc5ce",
+    "url": "/static/js/runtime-main.6b066d1b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stock-quantity-calculator/precache-manifest.e1ea1e3b2930d71346aeff7c71eff629.js"
+  "/precache-manifest.3fc0c46b7b4c6651ec045e64accc08e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stock-quantity-calculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
